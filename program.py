69e60b0abff34a47fb8f554ecd046b09a43b31ba
ronalds01 <ronalds01@gmail.com>
2023-11-18 18:04:22 -0600

    my first commit

---
+++ program.py
@@ -0,0 +1,9 @@
+my_list = [10,20,30,40,50,60]
+suma = sum(my_list)
+print("suma: ", suma)
+print(".....................................................")
+
+resultado = 1
+for elemento in my_list:
+    resultado *= elemento
+print("multiplicacion: ", resultado)
